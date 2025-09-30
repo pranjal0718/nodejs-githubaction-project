a9d14e19d0ab91fb8131f60189bd851db4d8444c
pranjal tiwari <Pranjalttiwarii@gmail.com>
2025-09-30 11:40:52 +0530

    changes in files

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello Pranjal");
+  res.send("I'm Pranjal, the Best Devops Enginner");
 });
 
 app.listen(3000, () => {
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("should return Hello World", async () => {
     const res = await request(app).get("/");
     expect(res.status).toBe(200);
-    expect(res.text).toBe("Hello Pranjal");
+    expect(res.text).toBe("I'm Pranjal, the Best Devops Enginner");
   });
 });
